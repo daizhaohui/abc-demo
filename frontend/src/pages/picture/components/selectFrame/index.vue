409d7cd898bdb005837782fa3b1f90c4760ca22c
daizhaohui <daizhaohui@vip.sina.com>
2022-10-21 22:44:16 +0800

    天剑选项框

---
+++ frontend/src/pages/picture/components/selectFrame/index.vue
@@ -0,0 +1,12 @@
+<template>
+ <div class="select-frame">
+    <div id="content" :style="contentStyle">
+      <div id="mg-content">
+        <img id="labeled-image" :src="image" />
+        <canvas id="canvas" ref="refCanvas"></canvas>
+      </div>
+    </div>
+ </div>
+</template>
+<script src="./index.ts" lang="ts"></script>
+<style lang="less" src='./index.less'></style>

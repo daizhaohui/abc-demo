8b4313e2f337a4915fda16ed5260274eefe2bd22
daizhaohui <daizhaohui@vip.sina.com>
2022-10-24 18:48:27 +0800

    update documents

--- backend/app.config.js
+++ backend/app.config.js
@@ -1,25 +1,24 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.default = {
-    db: {
-        type: 'mysql',
-        host: 'abc-demo-mysql',
-        port: 3306,
-        username: 'root',
-        password: 'abc-demo',
-        database: 'demo',
-        entities: ['dist/**/*.entity{.ts,.js}'],
-        synchronize: false,
-        retryDelay: 20000,
+  db: {
+    type: 'mysql',
+    host: 'abc-demo-mysql',
+    port: 3306,
+    username: 'root',
+    password: 'abc-demo',
+    database: 'demo',
+    entities: ['dist/**/*.entity{.ts,.js}'],
+    synchronize: false,
+    retryDelay: 20000,
+  },
+  aws: {
+    s3: {
+      url: 'https://daizhaohui-abc-demo.s3.ca-central-1.amazonaws.com/*',
+      bucket: 'daizhaohui-abc-demo',
+      accessKeyId: 'AKIA2AHBTNFPQ22P6PNJ',
+      secretAccessKey: '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',
     },
-    aws: {
-        s3: {
-            url: 'https://daizhaohui-abc-demo.s3.ca-central-1.amazonaws.com/*',
-            bucket: 'daizhaohui-abc-demo',
-            accessKeyId: 'AKIA2AHBTNFPQ22P6PNJ',
-            secretAccessKey: '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',
-        },
-    },
-    port: 8002,
+  },
+  port: 8002,
 };
-//# sourceMappingURL=app.config.js.map

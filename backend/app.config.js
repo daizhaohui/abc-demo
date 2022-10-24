c507c1b5f4d03a02d766bc9405748fde48a4c9b4
daizhaohui <daizhaohui@vip.sina.com>
2022-10-24 18:15:01 +0800

    ok

---
+++ backend/app.config.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    db: {
+        type: 'mysql',
+        host: 'abc-demo-mysql',
+        port: 3306,
+        username: 'root',
+        password: 'abc-demo',
+        database: 'demo',
+        entities: ['dist/**/*.entity{.ts,.js}'],
+        synchronize: false,
+        retryDelay: 20000,
+    },
+    aws: {
+        s3: {
+            url: 'https://daizhaohui-abc-demo.s3.ca-central-1.amazonaws.com/*',
+            bucket: 'daizhaohui-abc-demo',
+            accessKeyId: 'AKIA2AHBTNFPQ22P6PNJ',
+            secretAccessKey: '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',
+        },
+    },
+    port: 8002,
+};
+//# sourceMappingURL=app.config.js.map
